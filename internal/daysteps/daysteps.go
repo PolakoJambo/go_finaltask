649c1b736c3d11a5acc159d5b27dfcd1e8e15be1
Михаил <mseroov@mail.ru>
2026-04-27 22:55:27 +0300

    Исправление форматирования вывода

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -73,5 +73,5 @@ func DayActionInfo(data string, weight, height float64) string {
 		return ""
 	}
 
-	return fmt.Sprintf("Количество шагов: %d. \nДистанция составила %0.2f км.\nВы сожгли %0.2f ккал.", steps, distanceKm, calories)
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %0.2f км.\nВы сожгли %0.2f ккал.\n", steps, distanceKm, calories)
 }
--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -104,7 +104,7 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
 	}
 	hours := duration.Hours()
 
-	return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f", action, hours, dist, speed, calories), nil
+	return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", action, hours, dist, speed, calories), nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
